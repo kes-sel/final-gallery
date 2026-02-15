42995739753eaaf25ec1116760dc0e45884f1fdc
kes-sel <mandanaskessel@gmail.com>
2026-02-15 23:18:16 +0800

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,36 +1,39 @@
 /* gallery.js */
 
 function upDate(previewPic) {
-
     console.log("Event triggered: Updating display for " + previewPic.alt);
     
     let displayDiv = document.getElementById("image");
-  
+    
+    // Changing the background image
     displayDiv.style.backgroundImage = "url('" + previewPic.src + "')";
-  
+    
+    // Changing the text
     displayDiv.innerHTML = previewPic.alt;
 }
 
 function unDo() {
-
     console.log("Event triggered: Resetting display");
     
     let displayDiv = document.getElementById("image");
     
+    // Resetting background and text
     displayDiv.style.backgroundImage = "url('')";
-    
     displayDiv.innerHTML = "Hover over an image below to display here.";
 }
 
 function addTabFocus() {
-    console.log("Page loaded: Initializing tab indices for accessibility.");
+    console.log("Page loaded: Initializing tab indices.");
     
     let images = document.querySelectorAll(".preview");
   
     for (let i = 0; i < images.length; i++) {
         images[i].setAttribute("tabindex", "0");
-        console.log("Tabindex added to image " + (i + 1) + ": " + images[i].alt);
+        console.log("Tabindex added to image " + (i + 1));
     }
 }
 
-window.onload = addTabFocus;
+// Ensure the onload function is assigned correctly
+window.onload = function() {
+    addTabFocus();
+};
